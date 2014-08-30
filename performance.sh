4444daaceb531524d94e276671371a8bf07d9730
Ruilong Huo <Ruilong.Huo@emc.com>
2014-08-30 07:32:48 +0000

    Correct typo in script to start performance test
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 197671]

--- performance.sh
+++ performance.sh
@@ -3,6 +3,6 @@
 source ~/.bashrc
 source ~/hawq_env.sh
 
-pytohn -u lsp.py -s performance_full_regression_single_stream_tpch      > ./performance_full_regression_single_stream_tpch.log      2>&1
+python -u lsp.py -s performance_full_regression_single_stream_tpch      > ./performance_full_regression_single_stream_tpch.log      2>&1
 
 # python -u lsp.py -s performance_full_regression_concurrent_streams_tpch > ./performance_full_regression_concurrent_streams_tpch.log 2>&1
