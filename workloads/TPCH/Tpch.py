ff3e2143e79022df18587bf3c418859ce4d97754
Ruilong Huo <Ruilong.Huo@emc.com>
2014-09-03 09:48:16 +0000

    add table_setting in Workload.py
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 197779]

--- workloads/TPCH/Tpch.py
+++ workloads/TPCH/Tpch.py
@@ -32,108 +32,11 @@ except ImportError:
     sys.stderr.write('LSP needs shell in lib/Shell.py\n')
     sys.exit(2)
 
-try:
-    from lib.Config import config
-except ImportError:
-    sys.stderr.write('LSP needs config in lib/Config.py\n')
-    sys.exit(2)
-
 
 class Tpch(Workload):
     def __init__(self, workload_specification, workload_directory, report_directory, report_sql_file): 
         # init base common setting such as dbname, load_data, run_workload , niteration etc
         Workload.__init__(self, workload_specification, workload_directory, report_directory, report_sql_file)
-
-        # init tpch specific configuration such as tpch table_settings
-        ts = workload_specification['table_setting']
-
-        # Calculate scale factor for TPC-H workload
-        self.data_volume_type = ts['data_volume_type'].upper()
-        self.data_volume_size = ts['data_volume_size']
-        
-        # Need to make it univerally applicable instead of hard-code number of segments
-        self.nsegs =  config.getNPrimarySegments()
-
-        self.scale_factor = 1
-        if self.data_volume_type == 'TOTAL':
-            self.scale_factor = self.data_volume_size
-        elif self.data_volume_type == 'PER_NODE':
-            nnodes = len(config.getSegHostNames())
-            self.scale_factor = self.data_volume_size * nnodes
-        elif self.data_volume_type == 'PER_SEGMENT':
-            self.scale_factor = self.data_volume_size * self.nsegs
-        else:
-            self.output('Error in calculating data volumn for workloads %s: data_volume_type=%s, data_volume_size=%s' % (self.workload_name, self.data_volume_type, self.data_volume_size))
-            exit(-1)
-
-        # Parse table setting
-        ts_keys = ts.keys()
-
-        self.append_only = None
-        if 'append_only' in ts_keys:
-            self.append_only = ts['append_only']
-            assert self.append_only in [True, False]
-
-        self.orientation = 'ROW'
-        if 'orientation' in ts_keys:
-            self.orientation = ts['orientation'].upper()
-            assert self.orientation in ['PARQUET', 'ROW', 'COLUMN']
-
-        self.row_group_size = None
-        if 'row_group_size' in ts_keys:
-            self.row_group_size = int(ts['row_group_size'])
-            
-        self.page_size = None
-        if 'page_size' in ts_keys:
-            self.page_size = int(ts['page_size'])
-       
-        self.compression_type = None
-        if 'compression_type' in ts_keys:
-            self.compression_type = ts['compression_type'].upper()
-        
-        self.compression_level = None
-        if 'compression_level' in ts_keys:
-                self.compression_level = int(ts['compression_level'])
-
-        self.partitions = 0
-        if 'partitions' in ts_keys:
-            self.partitions = int(ts['partitions'])
-
-        # prepare name with suffix for table and corresponding sql statement to create it
-        tbl_suffix = ''
-        sql_suffix = ''
-
-        if self.append_only in [None, True]:
-            tbl_suffix = tbl_suffix + 'ao'
-            sql_suffix = sql_suffix + 'appendonly = true'
-        else:
-            tbl_suffix = tbl_suffix + 'heap'
-            sql_suffix = sql_suffix + 'appendonly = false'
-
-        tbl_suffix = tbl_suffix + '_' + self.orientation
-        sql_suffix = sql_suffix + ', '+ 'orientation = ' + self.orientation
-
-        if self.orientation == 'PARQUET':
-            sql_suffix = sql_suffix + ', ' + 'pagesize = %s, rowgroupsize = %s' % (self.page_size, self.row_group_size)
-
-        if self.compression_type is not None:
-            if self.compression_level is not None:
-                tbl_suffix = tbl_suffix + '_' + self.compression_type + str(self.compression_level)
-                sql_suffix = sql_suffix + ', ' + 'compresstype = ' + self.compression_type 
-                sql_suffix = sql_suffix + ', ' + 'compresslevel = ' + str(self.compression_level)
-            else:
-                tbl_suffix = tbl_suffix + '_' + self.compression_type
-                sql_suffix = sql_suffix + ', ' + 'compresstype = ' + self.compression_type
-        else:
-            tbl_suffix = tbl_suffix + '_nocomp'
-
-        if self.partitions > 0:
-            tbl_suffix += '_part'
-        else:
-            tbl_suffix += '_nopart'
-
-        self.tbl_suffix = tbl_suffix.lower()
-        self.sql_suffix = sql_suffix
 
     def setup(self):
         pass
--- workloads/Workload.py
+++ workloads/Workload.py
@@ -12,6 +12,12 @@ except ImportError:
     sys.exit(2)
 
 try:
+    from lib.Config import config
+except ImportError:
+    sys.stderr.write('LSP needs config in lib/Config.py\n')
+    sys.exit(2)
+
+try:
     from QueryFile import QueryFile
 except ImportError:
     sys.stderr.write('LSP needs QueryFile in lib/QueryFile.py\n')
@@ -42,7 +48,9 @@ class Workload(object):
         self.load_data_flag = str(workload_specification['load_data_flag']).strip().upper()
         self.run_workload_flag = str(workload_specification['run_workload_flag']).strip().upper()
         
-        self.table_setting = workload_specification['table_setting']
+        # get table setting
+        self.get_table_setting(workload_specification)
+
         self.run_workload_mode = workload_specification['run_workload_mode'].strip().upper()
         self.num_concurrency = int(str(workload_specification['num_concurrency']).strip())
         self.num_iteration = int(str(workload_specification['num_iteration']).strip())
@@ -96,6 +104,98 @@ class Workload(object):
 
         # should always run the workload by default
         self.should_stop = False
+
+    def get_table_setting(self, workload_specification):
+        # init tpch specific configuration such as tpch table_settings
+        ts = workload_specification['table_setting']
+
+        # Calculate scale factor for TPC-H workload
+        self.data_volume_type = ts['data_volume_type'].upper()
+        self.data_volume_size = ts['data_volume_size']
+        
+        # Need to make it univerally applicable instead of hard-code number of segments
+        self.nsegs =  config.getNPrimarySegments()
+
+        self.scale_factor = 1
+        if self.data_volume_type == 'TOTAL':
+            self.scale_factor = self.data_volume_size
+        elif self.data_volume_type == 'PER_NODE':
+            nnodes = len(config.getSegHostNames())
+            self.scale_factor = self.data_volume_size * nnodes
+        elif self.data_volume_type == 'PER_SEGMENT':
+            self.scale_factor = self.data_volume_size * self.nsegs
+        else:
+            self.output('Error in calculating data volumn for workloads %s: data_volume_type=%s, data_volume_size=%s' % (self.workload_name, self.data_volume_type, self.data_volume_size))
+            exit(-1)
+
+        # Parse table setting
+        ts_keys = ts.keys()
+
+        self.append_only = None
+        if 'append_only' in ts_keys:
+            self.append_only = ts['append_only']
+            assert self.append_only in [True, False]
+
+        self.orientation = 'ROW'
+        if 'orientation' in ts_keys:
+            self.orientation = ts['orientation'].upper()
+            assert self.orientation in ['PARQUET', 'ROW', 'COLUMN']
+
+        self.row_group_size = None
+        if 'row_group_size' in ts_keys:
+            self.row_group_size = int(ts['row_group_size'])
+            
+        self.page_size = None
+        if 'page_size' in ts_keys:
+            self.page_size = int(ts['page_size'])
+       
+        self.compression_type = None
+        if 'compression_type' in ts_keys:
+            self.compression_type = ts['compression_type'].upper()
+        
+        self.compression_level = None
+        if 'compression_level' in ts_keys:
+                self.compression_level = int(ts['compression_level'])
+
+        self.partitions = 0
+        if 'partitions' in ts_keys:
+            self.partitions = int(ts['partitions'])
+
+        # prepare name with suffix for table and corresponding sql statement to create it
+        tbl_suffix = ''
+        sql_suffix = ''
+
+        if self.append_only in [None, True]:
+            tbl_suffix = tbl_suffix + 'ao'
+            sql_suffix = sql_suffix + 'appendonly = true'
+        else:
+            tbl_suffix = tbl_suffix + 'heap'
+            sql_suffix = sql_suffix + 'appendonly = false'
+
+        tbl_suffix = tbl_suffix + '_' + self.orientation
+        sql_suffix = sql_suffix + ', '+ 'orientation = ' + self.orientation
+
+        if self.orientation == 'PARQUET':
+            sql_suffix = sql_suffix + ', ' + 'pagesize = %s, rowgroupsize = %s' % (self.page_size, self.row_group_size)
+
+        if self.compression_type is not None:
+            if self.compression_level is not None:
+                tbl_suffix = tbl_suffix + '_' + self.compression_type + str(self.compression_level)
+                sql_suffix = sql_suffix + ', ' + 'compresstype = ' + self.compression_type 
+                sql_suffix = sql_suffix + ', ' + 'compresslevel = ' + str(self.compression_level)
+            else:
+                tbl_suffix = tbl_suffix + '_' + self.compression_type
+                sql_suffix = sql_suffix + ', ' + 'compresstype = ' + self.compression_type
+        else:
+            tbl_suffix = tbl_suffix + '_nocomp'
+
+        if self.partitions > 0:
+            tbl_suffix += '_part'
+        else:
+            tbl_suffix += '_nopart'
+
+        self.tbl_suffix = tbl_suffix.lower()
+        self.sql_suffix = sql_suffix
 
     def setup(self):
         '''Setup prerequisites for workload'''
