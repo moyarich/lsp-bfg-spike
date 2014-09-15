9e8433ffc950a017624b617b2683b0f766160b27
Ruilong Huo <Ruilong.Huo@emc.com>
2014-09-15 09:21:21 +0000

    add Remotecommand in lib
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 198192]

---
+++ lib/RemoteCommand.py
@@ -0,0 +1,42 @@
+try:
+    import pexpect
+except ImportError:
+    sys.stderr.write('RemoteCommand needs pexpect\n')
+    sys.exit(2)
+
+class RemoteCommand:
+	def __init__(self):
+		pass
+
+	def ssh_command(self, user, host, password, command):
+   
+	    ssh_newkey = 'Are you sure you want to continue connecting'
+	    child = pexpect.spawn('ssh -l %s %s "%s"'%(user, host, command))
+	    i = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password: '])
+	    # Timeout
+	    if i == 0: 
+	        print 'ERROR!'
+	        print 'SSH could not login. Here is what SSH said:'
+	        print child.before, child.after
+	        return None
+	    # SSH does not have the public key. Just accept it.
+	    if i == 1: 
+	        child.sendline ('yes')
+	        child.expect ('password: ')
+	        i = child.expect([pexpect.TIMEOUT, 'password: '])
+	        # Timeout
+	        if i == 0: 
+	            print 'ERROR!'
+	            print 'SSH could not login. Here is what SSH said:'
+	            print child.before, child.after
+	            return None
+	    child.sendline(password)
+	    return child
+
+	def scp_command(self, from_user, from_host, password, to_user, to_host, from_file, to_file):
+	    child = pexpect.spawn('scp -r %s%s%s %s%s%s' %(from_user, from_host, from_file, to_user, to_host, to_file))
+	    child.expect('password:')
+	    child.sendline(password)
+	    return child
+
+remotecmd = RemoteCommand()
