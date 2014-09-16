067063ed5180e3b757afad0741345f43f07ff370
Ruilong Huo <Ruilong.Huo@emc.com>
2014-09-16 05:45:37 +0000

    update remotecmd
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 198228]

--- lib/RemoteCommand.py
+++ lib/RemoteCommand.py
@@ -4,12 +4,13 @@ except ImportError:
     sys.stderr.write('RemoteCommand needs pexpect\n')
     sys.exit(2)
 
+import sys
+
 class RemoteCommand:
 	def __init__(self):
 		pass
 
 	def ssh_command(self, user, host, password, command):
-   
 	    ssh_newkey = 'Are you sure you want to continue connecting'
 	    child = pexpect.spawn('ssh -l %s %s "%s"'%(user, host, command))
 	    i = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password: '])
@@ -34,11 +35,10 @@ class RemoteCommand:
 	    child.expect(pexpect.EOF)
 	    return child.before
 
-	def scp_command(self, from_user, from_host, password, to_user, to_host, from_file, to_file):
+	def scp_command(self, from_user, from_host, from_file, to_user, to_host, to_file, password):
 	    child = pexpect.spawn('scp -r %s%s%s %s%s%s' %(from_user, from_host, from_file, to_user, to_host, to_file))
 	    child.expect('password:')
 	    child.sendline(password)
 	    child.expect(pexpect.EOF)
-	    return child.before
 
 remotecmd = RemoteCommand()
