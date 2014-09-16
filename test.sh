fb1dd90986d1c36f4447635ac84f2b7bf954c910
Ruilong Huo <Ruilong.Huo@emc.com>
2014-09-16 03:06:19 +0000

    Cleanup code base
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 198220]

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python -u lsp.py -c 'HAWQ main performance on dca22' -s huor
-
-#python -u lsp.py -s performance_full_regression_single_stream_tpch      > ./performance_full_regression_single_stream_tpch.log      2>&1
-
