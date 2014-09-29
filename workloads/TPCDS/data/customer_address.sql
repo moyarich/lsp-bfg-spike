8e2e7085cb665f5f1f28291db88d475d919efb7f
Ruilong Huo <Ruilong.Huo@emc.com>
2014-09-29 08:01:54 +0000

    add TPCDS data folder
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 198757]

---
+++ workloads/TPCDS/data/customer_address.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS customer_address_TABLESUFFIX CASCADE;
+DROP EXTERNAL WEB TABLE IF EXISTS e_customer_address_TABLESUFFIX;
+
+create table customer_address_TABLESUFFIX
+(
+    ca_address_sk             integer               not null,
+    ca_address_id             char(16)              not null,
+    ca_street_number          char(10)                      ,
+    ca_street_name            varchar(60)                   ,
+    ca_street_type            char(15)                      ,
+    ca_suite_number           char(10)                      ,
+    ca_city                   varchar(60)                   ,
+    ca_county                 varchar(30)                   ,
+    ca_state                  char(2)                       ,
+    ca_zip                    char(10)                      ,
+    ca_country                varchar(20)                   ,
+    ca_gmt_offset             decimal(5,2)                  ,
+    ca_location_type          char(20)                     
+) WITH (SQLSUFFIX)PARTITIONS distributed by (ca_address_sk);
+
+CREATE EXTERNAL TABLE e_customer_address_TABLESUFFIX
+(
+ca_address_sk             integer               ,
+ca_address_id             char(16)              ,
+ca_street_number          char(10)                      ,
+ca_street_name            varchar(60)                   ,
+ca_street_type            char(15)                      ,
+ca_suite_number           char(10)                      ,
+ca_city                   varchar(60)                   ,
+ca_county                 varchar(30)                   ,
+ca_state                  char(2)                       ,
+ca_zip                    char(10)                      ,
+ca_country                varchar(20)                   ,
+ca_gmt_offset             decimal(5,2)                  ,
+ca_location_type          char(20)
+)
+LOCATION
+FORMAT 'TEXT' (DELIMITER '|' NULL AS '');
