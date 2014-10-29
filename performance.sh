28a40bd1b4de93dd4b42c683431a5babcad6e62d
Ruilong Huo <Ruilong.Huo@emc.com>
2014-10-29 02:48:27 +0000

    update performance.sh
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 209344]

--- performance.sh
+++ performance.sh
@@ -3,10 +3,10 @@
 source ~/.bashrc
 source ~/qa.sh
 
-# python -u lsp.py -s performance_full_regression_single_stream_tpch,performance_full_regression_concurrent_streams_tpch > ./performance_full_regression_tpch.log 2>&1
+# python -u lsp.py -s performance_full_regression_single_stream_tpch,performance_full_regression_concurrent_streams_tpch -a > ./performance_full_regression_tpch.log 2>&1
 
-# python -u lsp.py -s performance_full_regression_single_stream_tpch > ./performance_full_regression_tpch.log 2>&1
+# python -u lsp.py -s performance_full_regression_single_stream_tpch -a > ./performance_full_regression_tpch.log 2>&1
 
-# python -u lsp.py -s performance_sanity > ./performance_sanity.log 2>&1
+# python -u lsp.py -s performance_sanity -a > ./performance_sanity.log 2>&1
 
-python -u lsp.py -s tpcds > ./tpcds.log 2>&1
+python -u lsp.py -s tpcds -a > ./tpcds.log 2>&1
