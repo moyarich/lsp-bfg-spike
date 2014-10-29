0e6101421d8795e76870a261a6045c372a1bfadf
Ruilong Huo <Ruilong.Huo@emc.com>
2014-10-29 05:14:03 +0000

    add stress.sh
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 209359]

---
+++ stress.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+source ~/.bashrc
+source ~/qa.sh
+
+### Daily stress test 
+# python -u lsp.py -s system_load -b > ./system_load.log 2>&1 
+# sleep 10
+# nohup python -u lsp.py -s system_run_random_2_iteration_2_concurrency -b -c > ./system_run_random_2_iteration_2_concurrency.log 2>&1 &
+
+
+### QUAYL stress test
+python -u lsp.py -s stress_load > ./stress_load.log 2>&1 
+sleep 10
+nohup python -u lsp.py -s stress_run > ./stress_run.log 2>&1 &
