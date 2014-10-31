f4bf22b933fdfc5fe5d4932a7c8879c145c6de1c
Ruilong Huo <Ruilong.Huo@emc.com>
2014-10-31 04:16:21 +0000

    print sql when remote run sql
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 209518]

--- lib/utils/Check.py
+++ lib/utils/Check.py
@@ -21,15 +21,18 @@ def remote_psql_file(sql, user, host, password, local_file = '/tmp/temp.sql', re
 
 
 class Check:
-    def __init__(self):
-        self.user = 'gpadmin'
-        self.host = 'gpdb63.qa.dh.greenplum.com'
-        self.password = 'changeme'
+    def __init__(self, user = 'gpadmin', host = 'gpdb63.qa.dh.greenplum.com', password = 'changeme'):
+        self.user = user
+        self.host = host
+        self.password = password
 
     def check_id(self, result_id, table_name, search_condition):
         sql = 'select %s from %s where %s;' % (result_id, table_name, search_condition)
         result = remote_psql_file(sql = sql, user = self.user, host = self.host, password = self.password)
         result = str(result).strip()
+        if result.find('ERROR') != -1 or result.find('FATAL') != -1:
+            print 'remote sql file wrong: \n' + result + '\n the sql is:' + sql 
+            sys.exit(2)
         if result.isdigit():
             return int(result)
         else:
@@ -37,8 +40,12 @@ class Check:
 
     def get_max_id(self, result_id, table_name):
         sql = 'select max(%s) from %s ;' % (result_id, table_name)
+        print sql
         result = remote_psql_file(sql = sql, user = self.user, host = self.host, password = self.password)
         result = str(result).strip()
+        if result.find('ERROR') != -1 or result.find('FATAL') != -1:
+            print 'remote sql file wrong: \n' + result
+            sys.exit(2)
         if result.isdigit():
             return int(result)
         else:
@@ -49,17 +56,21 @@ class Check:
             sql = "Insert into %s values (%s);" % (table_name, values)
         else:
             sql = "Insert into %s (%s) values (%s);" % (table_name, col_list, values)
+        print sql
         remote_psql_file(sql = sql, user = self.user, host = self.host, password = self.password)
 
     def update_record(self, table_name, set_content = '', search_condition = ''):
         sql = "update %s set %s where %s;" % (table_name, set_content, search_condition)
+        print sql
         remote_psql_file(sql = sql, user = self.user, host = self.host, password = self.password)
         
     def get_result(self, col_list = '',table_list = '', search_condition = ''):
         sql = "select %s from %s %s;" % (col_list, table_list, search_condition)
+        print sql
         return remote_psql_file(sql = sql, user = self.user, host = self.host, password = self.password)
 
     def get_result_by_sql(self, sql = ''):
+        print sql
         return remote_psql_file(sql = sql, user = self.user, host = self.host, password = self.password)
 
 check = Check()
