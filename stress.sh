b092f2abf22aad0cfdd31815f67bde55f12fcd9f
Ruilong Huo <Ruilong.Huo@emc.com>
2014-11-03 01:56:03 +0000

    only load tpch_parquet_snappy_part_200g
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 209560]

--- stress.sh
+++ stress.sh
@@ -11,5 +11,5 @@ source ~/qa.sh
 
 ### QUAYL stress test
 python -u lsp.py -s stress_load > ./stress_load.log 2>&1 
-sleep 10
-nohup python -u lsp.py -s stress_run > ./stress_run.log 2>&1 &
+#sleep 10
+#nohup python -u lsp.py -s stress_run > ./stress_run.log 2>&1 &
