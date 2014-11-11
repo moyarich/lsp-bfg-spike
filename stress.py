5112cde243ced7a32d4e06b507ee5ea710fcf9d0
Ruilong Huo <Ruilong.Huo@emc.com>
2014-11-11 03:17:48 +0000

    update stress.py
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 209908]

--- stress.py
+++ stress.py
@@ -153,7 +153,7 @@ class Check_hawq_stress():
         for host in self.hawq_segments:
             for log_path in self.hawq_config[host]:
                 cmd = ''' gpssh -h %s -e "cd %s; source greenplum_path.sh; gplogfilter -b '%s' -e '%s' -m '%s' %s "''' % (host, gphome, bt, et, searchKeyRegrex, log_path + '/pg_log/*.csv')
-                print cmd
+                #print cmd
                 (status, output) = commands.getstatusoutput(cmd)
                 
                 if status != 0:
