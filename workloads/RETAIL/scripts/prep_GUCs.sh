1f91f8f99e85402944381e883d75e5036545edd4
Ruilong Huo <Ruilong.Huo@emc.com>
2014-11-13 05:58:55 +0000

    update2 about retail workload
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 209976]

--- workloads/RETAIL/scripts/prep_GUCs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# prep_GUCs.sh
-
-# This script changes any default GUCs for optimal demo performance
-
-gpconfig -c max_connections -v 500 -m 100
-gpconfig -c gp_segment_connect_timeout -v 600
-gpstop -u
