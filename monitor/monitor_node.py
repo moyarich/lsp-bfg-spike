49f07f2d304f6d0dddac5b7417b68faca528c8f3
Ruilong Huo <Ruilong.Huo@emc.com>
2014-12-18 03:46:26 +0000

    add top method in monitor node
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 210486]

--- monitor/monitor_node.py
+++ monitor/monitor_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os,sys,commands,time
 from datetime import datetime
+import subprocess
 
 class Monitor_node():
 
@@ -12,11 +13,58 @@ class Monitor_node():
 	def report(self, filename, msg):
 		if msg != '':
 		    fp = open(filename, 'a')  
-		    fp.write(msg)
+		    fp.write(msg)               
 		    fp.write('\n')
 		    fp.flush()
 		    fp.close()
 
+	'''
+	 PID    USER    PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND
+	30705 gpadmin   39  19  528m  12m 6408 S  0.0  0.3   0:00.00 postgres: port 40000, gpadmin gpsqltest_tpch_ao_row_gpadmin 127.0.0.1(38011) con1140 seg6 cmd2 slice4 MPPEXEC SELECT
+	35019 gpadmin   39  19  526m 9944 5964 S  0.0  0.3   0:00.00 postgres: port 40000, gpadmin gpsqltest_tpch_ao_row_gpadmin 127.0.0.1(40991) con1376 seg0 cmd6 MPPEXEC INSERT                                                                                                                                                   
+    35021 gpadmin   39  19  526m 9.8m 5996 S  0.0  0.3   0:00.00 postgres: port 40000, gpadmin gpsqltest_tpch_ao_row_gpadmin 127.0.0.1(40992) con1376 seg0 cmd6 slice1 MPPEXEC INSERT                                                                                                                                            
+	30713 gpadmin   39  19  526m  10m 6604 S  0.0  0.3   0:00.00 postgres: port 40000, gpadmin gpsqltest_tpch_ao_row_gpadmin 127.0.0.1(38015) con1140 seg3 idle 
+index 0      1      2   3    4     5   6   7   8    9      10     11         12   13     14              15                       16            17     18   19    20     21      22
+	'''
+	def __get_qe_mem_by_top(self):
+		filter_string = 'bin/postgres|logger|stats|writer|checkpoint|seqserver|WAL|ftsprobe|sweeper|sh -c|bash|grep|seg-|resource manager'
+		grep_string1 = 'postgres'
+		grep_string2 = 'seg'
+		cmd = ''' top -n 1 -b -c | grep postgres | grep seg | grep -vE "bin/postgres|logger|stats|writer|checkpoint|seqserver|WAL|ftsprobe|sweeper|sh -c|bash|grep|resource manager" ''' 
+		p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+		output, error = p.communicate()
+		
+		if error is not None or output == '':
+			print 'error: ' + str(error) + ' output: ' + output + 'in qe_mem_cpu'
+			return None
+		
+		line_item = output.splitlines()
+		now_time = str(datetime.now())
+		output_string = ['', '']
+		
+		for line in line_item:
+			temp = line.split()
+			if len(temp) < 20:
+				continue
+
+			# hostname, count, time_point, pid, con_id, seg_id, cmd, slice, status, rss, pmem, pcpu
+			if temp[19] == 'idle':
+				one_item = self.hostname + '\t' + str(self.count) + '\t' + now_time + '\t' + temp[0] + '\t' + temp[17] + '\t' + temp[18] + '\t' + temp[19] + '\t' + 'NUll' + '\t' + 'NULL' + '\t' + temp[5]+ '\t' + temp[9] + '\t' + temp[8]
+			elif temp[20].find('slice') != -1:
+				one_item = self.hostname + '\t' + str(self.count) + '\t' + now_time + '\t' + temp[0] + '\t' + temp[17] + '\t' + temp[18] + '\t' + temp[19] + '\t' + temp[20] + '\t' + temp[22] + '\t' + temp[5] + '\t' + temp[9] + '\t' + temp[8]
+			else:
+				one_item = self.hostname + '\t' + str(self.count) + '\t' + now_time + '\t' + temp[0] + '\t' + temp[17] + '\t' + temp[18] + '\t' + temp[19 ] + '\t' + 'NULL' + '\t' + temp[21] + '\t' + temp[5] + '\t' + temp[9] + '\t' + temp[8]
+
+			col_item = one_item.split('\t')
+
+			sql_item = "insert into moni.qe_mem_cpu values ('%s', %s, '%s', %s, %s, '%s', '%s', '%s', '%s', %s, %s, %s);" \
+				% (col_item[0], col_item[1], col_item[2], col_item[3], col_item[4][3:], col_item[5], col_item[6], col_item[7], col_item[8], col_item[9], col_item[10], col_item[11])
+
+			output_string[0] = output_string[0] + '\n' + one_item
+			output_string[1] = output_string[1] + '\n' + sql_item
+		self.count = self.count + 1
+
+		return output_string
 
 	'''
 	   pid %CPU  VSZ  RSS  %MEM STATE CMD          
@@ -27,7 +75,7 @@ class Monitor_node():
 index  0    1      2     3     4  5    6       7     8       9             10                        11           12     13  14    15      16     17
 	'''
 	
-	def __get_qe_mem(self):
+	def __get_qe_mem_by_ps(self):
 		filter_string = 'bin/postgres|logger|stats|writer|checkpoint|seqserver|WAL|ftsprobe|sweeper|sh -c|bash|grep|seg-|resource manager'
 		grep_string1 = 'postgres'
 		grep_string2 = 'seg'
@@ -68,7 +116,7 @@ index  0    1      2     3     4  5    6       7     8       9             10   
 	def get_qe_mem(self, filename = ['', ''], interval = 5):
 		count = 0
 		while(count < 10):
-			result = self.__get_qe_mem()
+			result = self.__get_qe_mem_by_top()
 			if result is None:
 				count = count + 1
 				time.sleep(2)
