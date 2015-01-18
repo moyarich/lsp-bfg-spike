b887e817b9d2552a6c5253bae10d86f2976989a9
Ruilong Huo <Ruilong.Huo@emc.com>
2015-01-18 06:10:28 +0000

    fix abug about tpch load data: region table
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 210884]

--- workloads/TPCH/data/region.sql
+++ workloads/TPCH/data/region.sql
@@ -12,7 +12,7 @@ CREATE EXTERNAL WEB TABLE e_region_TABLESUFFIX (
     R_REGIONKEY  INTEGER,
     R_NAME       CHAR(25),
     R_COMMENT    VARCHAR(152) )
-EXECUTE E'bash -c "$GPHOME/bin/dbgen -b $GPHOME/bin/dists.dss -T r -s SCALEFACTOR"' ---N NUMSEGMENTS -n $((GP_SEGMENT_ID + 1))"' 
+EXECUTE E'bash -c "$GPHOME/bin/dbgen -b $GPHOME/bin/dists.dss -T r -s SCALEFACTOR"'
 ON 1 FORMAT 'TEXT' (DELIMITER '|');
 
 INSERT INTO region_TABLESUFFIX SELECT * FROM e_region_TABLESUFFIX;
