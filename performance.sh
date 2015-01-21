dce97afbecf3a2bc6dafbc6cbd0704923389a159
Ruilong Huo <Ruilong.Huo@emc.com>
2015-01-21 03:43:12 +0000

    update
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 210928]

--- performance.sh
+++ performance.sh
@@ -10,15 +10,15 @@ source ~/qa.sh
 # python -u lsp.py -s performance_sanity -a > ./performance_sanity.log 2>&1
 
 #python -u lsp.py -s tpcds -a > ./tpcds.log 2>&1
-set split_read_size_mb = 512;
-gpstop -ar
+gpconfig -c split_read_size_mb -v 512 --skipvalidation
+gpstop -u
 python -u lsp.py -s performance1 -m 5 -a > ./performance1.log 2>&1
 sleep 10
-set split_read_size_mb = 8192;
-gpstop -ar
-python -u lsp.py -s performance2 -m 30 -a > ./performance2.log 2>&1
-sleep 10
-python -u lsp.py -s performance22 -m 10 -a > ./performance22.log 2>&1
-sleep 10
-python -u lsp.py -s performance3 -m 60 -a -r > ./performance3.log 2>&1
+#set split_read_size_mb = 8192;
+#gpstop -ar
+#python -u lsp.py -s performance2 -m 30 -a > ./performance2.log 2>&1
+#sleep 10
+#python -u lsp.py -s performance22 -m 10 -a > ./performance22.log 2>&1
+#sleep 10
+#python -u lsp.py -s performance3 -m 60 -a -r 4 > ./performance3.log 2>&1
 #python -u lsp.py -s performance1,performance2,performance3 -a > ./performance.log 2>&1
