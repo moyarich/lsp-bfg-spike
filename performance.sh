0c60565ed84c60ea6cf3fbf8485e954545c6c26c
Ruilong Huo <Ruilong.Huo@emc.com>
2015-01-27 08:01:11 +0000

    update
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 211050]

--- performance.sh
+++ performance.sh
@@ -8,13 +8,13 @@ source ~/qa.sh
 # python -u lsp.py -s performance_full_regression_single_stream_tpch -a > ./performance_full_regression_tpch.log 2>&1
 
 # python -u lsp.py -s performance_sanity -a > ./performance_sanity.log 2>&1
-ps -ef | grep postgres > ps.log 2>&1
-echo $GPHOME >> ps.log 2>&1
+
 gpconfig -c hash_dist_init_segment_num -v 64 --skipvalidation > gpconfig.log 2>&1
+gpconfig -c util_segment_num -v 64 --skipvalidation >> gpconfig.log 2>&1
 gpstop -ar >> gpconfig.log 2>&1
 #gpconfig -c split_read_size_mb -v 512 --skipvalidation
 #gpstop -u
-python -u lsp.py -s performance1 -a -c > ./performance1.log 2>&1
+python -u lsp.py -s performance1 -m 5 -a -c > ./performance1.log 2>&1
 #sleep 10
 #set split_read_size_mb = 8192;
 #gpstop -ar
