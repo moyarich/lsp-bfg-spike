7c6054e650cf05c7fded7117fcaab6801fde23ef
Ruilong Huo <Ruilong.Huo@emc.com>
2015-01-27 02:50:22 +0000

    2.0 performance
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 211036]

--- performance.sh
+++ performance.sh
@@ -8,18 +8,19 @@ source ~/qa.sh
 # python -u lsp.py -s performance_full_regression_single_stream_tpch -a > ./performance_full_regression_tpch.log 2>&1
 
 # python -u lsp.py -s performance_sanity -a > ./performance_sanity.log 2>&1
-
+gpconfig -c hash_dist_init_segment_num -v 64 --skipvalidation
+gpstop -ar
 #gpconfig -c split_read_size_mb -v 512 --skipvalidation
 #gpstop -u
-#python -u lsp.py -s performance1 -m 5 -a -c > ./performance1.log 2>&1
+python -u lsp.py -s performance1 -a -c > ./performance1.log 2>&1
 #sleep 10
 #set split_read_size_mb = 8192;
 #gpstop -ar
-#python -u lsp.py -s performance2 -m 30 -a -c > ./performance2.log 2>&1
+#python -u lsp.py -s performance2 -a -c > ./performance2.log 2>&1
 #sleep 10
-#python -u lsp.py -s performance3 -m 10 -a > ./performance3.log 2>&1
+#python -u lsp.py -s performance3 -a > ./performance3.log 2>&1
 #sleep 10
-python -u lsp.py -s performance4 -a -c > ./performance4.log 2>&1
-sleep 10
-python -u lsp.py -s performance5 -a -c > ./performance5.log 2>&1
+#python -u lsp.py -s performance4 -a -c > ./performance4.log 2>&1
+#sleep 10
+#python -u lsp.py -s performance5 -a -c > ./performance5.log 2>&1
 #python -u lsp.py -s performance1,performance2,performance3 -a > ./performance.log 2>&1
