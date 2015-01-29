bc0aa30e03adfb0601691d646fe4c5fde902b8a0
Jiali Yao <Jiali.Yao@emc.com>
2015-01-29 08:27:23 +0000

    Add report schedule
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 211136]

--- performance.sh
+++ performance.sh
@@ -14,13 +14,13 @@ gpconfig -c util_segment_num -v 64 --skipvalidation >> gpconfig.log 2>&1
 gpstop -ar >> gpconfig.log 2>&1
 #gpconfig -c split_read_size_mb -v 512 --skipvalidation
 #gpstop -u
-python -u lsp.py -s performance1 -a -c > ./performance1.log 2>&1
+python -u lsp.py -s performance1 -a -c -m 5 > ./performance1.log 2>&1
 sleep 10
-python -u lsp.py -s performance2 -a -c -r 2 > ./performance2.log 2>&1
+python -u lsp.py -s performance2 -a -c -m 30 -r 2 > ./performance2.log 2>&1
 #sleep 10
-#python -u lsp.py -s performance3 -a > ./performance3.log 2>&1
+#python -u lsp.py -s performance3 -a -m 10 > ./performance3.log 2>&1
 #sleep 10
-#python -u lsp.py -s performance4 -a -c > ./performance4.log 2>&1
+#python -u lsp.py -s performance4 -a -c -m 60 > ./performance4.log 2>&1
 #sleep 10
 #python -u lsp.py -s performance5 -a -c > ./performance5.log 2>&1
 #python -u lsp.py -s performance1,performance2,performance3 -a > ./performance.log 2>&1
