a8475d59df97e1346d108bf471cfe7259850437c
Ruilong Huo <Ruilong.Huo@emc.com>
2015-01-30 06:30:23 +0000

    update
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 211172]

--- performance_full.sh
+++ performance_full.sh
@@ -10,4 +10,3 @@ sleep 10
 python -u lsp.py -s performance_xmarq_200g -m 10 -a > ./performance_xmarq_200g.log 2>&1
 sleep 10
 python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 4 > ./performance_tpch_concurrent.log 2>&1
- 
