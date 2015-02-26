461f4dd2f81166727e26b20690d786560fc6cd98
Jiali Yao <Jiali.Yao@emc.com>
2015-02-26 05:33:20 +0000

    Refine schedule
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 211600]

--- performance_full.sh
+++ performance_full.sh
@@ -9,4 +9,7 @@ python -u lsp.py -s performance_tpch_200g -m 30 -a -c > ./performance_tpch_200g.
 sleep 10
 python -u lsp.py -s performance_xmarq_200g -m 10 -a > ./performance_xmarq_200g.log 2>&1
 sleep 10
+python -u lsp.py -s performance_tpch_stream -m 60 -a -c > ./performance_tpch_stream.log 2>&1
+sleep 10
 python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 4 > ./performance_tpch_concurrent.log 2>&1
+
