35b5a4aee0ea0a5474201ba26043c3c21f0f2149
Jiali Yao <Jiali.Yao@emc.com>
2015-03-23 05:56:05 +0000

    Fix typo error
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 225717]
    
    
    Former-commit-id: 5b5d926834728bc46ebba54ff29a1ddc30e43199

--- performance_sanity.sh
+++ performance_sanity.sh
@@ -3,7 +3,7 @@
 source ~/.bashrc
 source ~/qa.sh
 
-python -u lsp.py -s performance_tpch_10g -m 5 -a -c -r 1> ./performance_tpch_10g.log 2>&1
+python -u lsp.py -s performance_tpch_10g -m 5 -a -c -r 1 > ./performance_tpch_10g.log 2>&1
 #sleep 10
 #python -u lsp.py -s performance_tpch_200g -m 30 -a -c -r 2 > ./performance_tpch_200g.log 2>&1
 #sleep 10
