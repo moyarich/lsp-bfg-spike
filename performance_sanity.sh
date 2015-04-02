51617a33746bfabc668f6d23fd3ebc1f6b50eac6
Jiali Yao <Jiali.Yao@emc.com>
2015-04-02 08:22:31 +0000

    run performance with 10g/200g
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 225988]
    
    
    Former-commit-id: 43ae1680368d594146aaa7403b52b18dd68578c9

--- performance_sanity.sh
+++ performance_sanity.sh
@@ -6,9 +6,9 @@ source ~/qa.sh
 hawqconfig -c -n hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
 hawq cluster stop
 hawq cluster start
-python -u lsp.py -s performance_tpch_10g -m 5 -a -c -r 1 > ./performance_tpch_10g.log 2>&1
-#sleep 10
-#python -u lsp.py -s performance_tpch_200g -m 30 -a -c -r 2 > ./performance_tpch_200g.log 2>&1
+python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
+sleep 10
+python -u lsp.py -s performance_tpch_200g -m 30 -a -c -r 2 > ./performance_tpch_200g.log 2>&1
 #sleep 10
 #python -u lsp.py -s performance_xmarq_200g -m 10 -a > ./performance_xmarq_200g.log 2>&1
 #sleep 10
