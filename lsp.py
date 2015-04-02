2a860bb0ddb1e399f56304574f0e6adc3b421760
Jiali Yao <Jiali.Yao@emc.com>
2015-04-02 00:52:26 +0000

    refine baseline
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 225978]
    
    
    Former-commit-id: 818661100c878a3a6e1eef3c3e967ed837c9e515

--- lsp.py
+++ lsp.py
@@ -251,7 +251,7 @@ if __name__ == '__main__':
         if report_num > 0:
             start_run_id = int(tr_id) - int(report_num) + 1
             sql = "select wl_name, action_type,overral_test_result,  improvenum, passnum, failurenum, skipnum, errornum, actual_total_execution_time,baseline_total_execution_time,deviation \
-            from hst.f_generate_test_report_summary(%d, %d, 'PHD 2.2 build 59', 'HAWQ 2.0.0.0 build 12988 GVA ORCA OFF') where wl_name not like '%s' order by tr_id, s_id,action_type;" % (start_run_id, tr_id, '%' + 'RWITHD' + '%')
+            from hst.f_generate_test_report_summary(%d, %d, 'PHD 2.2 build 59', 'HAWQ 2.0.0.0 build 12988 GVA ORCA OFF') where wl_name not like '%s' order by action_type, tr_id, s_id;" % (start_run_id, tr_id, '%' + 'RWITHD' + '%')
 
             result = check.get_result_by_sql(sql = sql)
             result = str(result).strip().split('\r\n')
@@ -261,7 +261,7 @@ if __name__ == '__main__':
                 Report('./report/summary_report.txt' , msg)
 
             sql = "select wl_name, action_type,overral_test_result,  improvenum, passnum, failurenum, skipnum, errornum, actual_total_execution_time,baseline_total_execution_time,deviation \
-            from hst.f_generate_test_report_summary(%d, %d, 'PHD 2.2 build 59', 'HAWQ 1.2.1.2 build 11946 GVA ORCA OFF') where wl_name not like '%s' order by tr_id, s_id,action_type;" % (start_run_id, tr_id, '%' + 'RWITHD' + '%')
+            from hst.f_generate_test_report_summary(%d, %d, 'PHD 2.2 build 59', 'HAWQ 1.2.1.2 build 11946 GVA ORCA OFF') where wl_name not like '%s' order by action_type, tr_id, s_id;" % (start_run_id, tr_id, '%' + 'RWITHD' + '%')
 
             result = check.get_result_by_sql(sql = sql)
             result = str(result).strip().split('\r\n')
