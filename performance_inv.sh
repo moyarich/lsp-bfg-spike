ece6ea4d16538d82f10902c55d5225372d7fc48c
Jiali Yao <Jiali.Yao@emc.com>
2015-04-10 07:59:06 +0000

    add guc value
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226068]
    
    
    Former-commit-id: d6fc5814a0b80e8b694e35dcd6c531489d0268d6

--- performance_inv.sh
+++ performance_inv.sh
@@ -4,15 +4,11 @@ source ~/.bashrc
 source ~/qa.sh
 
 hawqconfig -c -n hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawqconfig -c -n coredump_on_memerror -v true
+hawqconfig -c -n gp_vmem_protect_limit -v 32768
 hawq cluster stop
 hawq cluster start
-python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
+python -u lsp.py -s performance_tpch_10g  > ./performance_tpch_10g.log 2>&1
 sleep 10
-python -u lsp.py -s performance_tpch_200g_full -m 30 -a -c > ./performance_tpch_200g.log 2>&1
-sleep 10
-python -u lsp.py -s performance_xmarq_200g -m 10 -a > ./performance_xmarq_200g.log 2>&1
-sleep 10
-python -u lsp.py -s performance_tpch_stream -m 60 -a -c > ./performance_tpch_stream.log 2>&1
-sleep 10
-python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 5 > ./performance_tpch_concurrent.log 2>&1
+python -u lsp.py -s performance_tpch_200g_inv > ./performance_tpch_200g.log 2>&1
 
