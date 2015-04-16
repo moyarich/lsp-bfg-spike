a75d00411b0ac3995c66cdcf2c95585e4466e5ef
Jiali Yao <Jiali.Yao@emc.com>
2015-04-16 05:51:41 +0000

    apply on new config
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226145]
    
    
    Former-commit-id: 094d7c5976409b846a4246620887e0134e898ff0

--- performance_full.sh
+++ performance_full.sh
@@ -3,7 +3,7 @@
 source ~/.bashrc
 source ~/qa.sh
 
-hawqconfig -c -n hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
 hawq cluster stop
 hawq cluster start
 python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
--- performance_inv.sh
+++ performance_inv.sh
@@ -3,8 +3,8 @@
 source ~/.bashrc
 source ~/qa.sh
 
-hawqconfig -c -n hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
-hawqconfig -c -n coredump_on_memerror -v true
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawqconfig -c coredump_on_memerror -v true
 hawq cluster stop
 hawq cluster start
 python -u lsp.py -s tpcds  > ./tpcds.log 2>&1
