7c44f20444f70848db859fddb825ac53011751ed
Jiali Yao <Jiali.Yao@emc.com>
2015-05-02 02:34:43 +0000

    refine workload
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226500]
    
    
    Former-commit-id: 8f3f21803e78c756e918ba29eb0648f0fecbcf16

--- performance_run.sh
+++ performance_run.sh
@@ -3,4 +3,7 @@
 source ~/.bashrc
 source ~/qa.sh
 
-python -u lsp.py -s performance_run  -a -c  > ./performance_tpch_run.log 2>&1
+hawqconfig -c default_segment_num -v 64
+hawq stop cluster -a
+hawq start cluster -a
+python -u lsp.py -s performance_run -a -c  > ./performance_tpch_run.log 2>&1
