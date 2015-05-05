eb517ae75522fe54cc449eed579b8273f51abff4
Jiali Yao <Jiali.Yao@emc.com>
2015-05-05 06:56:24 +0000

    Do not run in db mode for bcn cluster
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226571]
    
    
    Former-commit-id: 0bfedaaa0bf1b8fc33d8fe0f5198741ce71e9422

---
+++ performance_full_bcn.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source ~/.bashrc
+source ~/qa.sh
+
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawq stop cluster -a
+hawq start cluster -a
+python -u lsp.py -s performance_tpch_10g -a -c  > ./performance_tpch_10g.log 2>&1
+sleep 10
+python -u lsp.py -s performance_tpch_200g_full  -c > ./performance_tpch_200g.log 2>&1
+sleep 10
+python -u lsp.py -s performance_xmarq_200g  > ./performance_xmarq_200g.log 2>&1
+sleep 10
+python -u lsp.py -s performance_tpch_stream  -c > ./performance_tpch_stream.log 2>&1
+sleep 10
+python -u lsp.py -s performance_tpch_concurrent  -c  > ./performance_tpch_concurrent.log 2>&1
+
