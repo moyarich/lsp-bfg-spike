64ac5029d6a3b6d551f59a73807145365e4ed31b
Jiali Yao <Jiali.Yao@emc.com>
2015-05-05 09:26:11 +0000

    enable cgroup
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226573]
    
    
    Former-commit-id: 282d706e25dc80f2e03e8f787e4c1baaf5f88cf0

--- performance_full.sh
+++ performance_full.sh
@@ -4,6 +4,7 @@ source ~/.bashrc
 source ~/qa.sh
 
 hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawqconfig -c hawq_resourceenforcer_cpu_enable -v true
 hawq stop cluster -a
 hawq start cluster -a
 python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
