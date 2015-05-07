916c6f7da508de8718219dcfb3bcf8b0330e141d
Jiali Yao <Jiali.Yao@emc.com>
2015-05-07 06:55:13 +0000

    add sanity for bcn
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226609]
    
    
    Former-commit-id: 5696274b39701b7cdb48faa9fe0095ac98fab33d

---
+++ performance_sanity_bcn.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+source ~/.bashrc
+source ~/qa.sh
+
+hawqconfig -c default_segment_num -v 64
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawq stop cluster -a
+hawq start cluster -a
+python -u lsp.py -s performance_tpch_10g  > ./performance_tpch_10g.log 2>&1
+sleep 10
+python -u lsp.py -s performance_tpch_200g > ./performance_tpch_200g.log 2>&1
+#sleep 10
+#python -u lsp.py -s performance_xmarq_200g -m 10 -a > ./performance_xmarq_200g.log 2>&1
+#sleep 10
+#python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 4 > ./performance_tpch_concurrent.log 2>&1
