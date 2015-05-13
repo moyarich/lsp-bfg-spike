4093d9edadb652570da78466ed8bdcca29b2bf08
Jiali Yao <Jiali.Yao@emc.com>
2015-05-13 10:32:08 +0000

    Apply new workload
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226726]
    
    
    Former-commit-id: 565401100ba7f2f06eb6bff106d58541a1c81bd7

--- performance_sanity.sh
+++ performance_sanity.sh
@@ -3,31 +3,25 @@
 source ~/.bashrc
 source ~/qa.sh
 
-
-
-hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
-hawqconfig -c default_segment_num -v 64
-hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
-hawq stop cluster -a
-hawq start cluster -a
-psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
-python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
-sleep 10
-python -u lsp.py -s performance_tpch_200g -m 30 -a -c -r 2 > ./performance_tpch_200g.log 2>&1
-mkdir report/summary_bak
-mv report/summary_report*  report/summary_bak/
-
 hawqconfig -c default_segment_num -v 128
 hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 8
-#hawqconfig -c hawq_resourceenforcer_cpu_enable -v true
+hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
+
 hawq stop cluster -a
 hawq start cluster -a
-psql -d postgres -c "drop table if exists test; creatdde table test(a int); insert into test values (1);"
+psql -d postgres -c "select * from gp_segment_configuration;" >config
+hawqconfig -s default_segment_num >>config
+hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
+hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
+psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
+sleep 10
 python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
 sleep 10
 python -u lsp.py -s performance_tpch_200g -m 30 -a -c  > ./performance_tpch_200g.log 2>&1
 sleep 10
-python -u lsp.py -s performance_tpch_stream_sanity -c -r 3 > ./performance_tpch_stream.log 2>&1
+python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 3 > ./performance_tpch_concurrent.log 2>&1
 sleep 10
-python -u lsp.py -s resourcequene_tpch_ratio_10g  -c > ./resourcequene_tpch_ratio_10g 2>&1
+#4. 2. 1 ratio concurrent to run tpch 10G 
+python -u lsp.py -s resourcequene_tpch_ratio_10g -m 30 -a -c  > ./resourcequene_tpch_ratio_10g 2>&1
+
 
