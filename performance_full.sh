99721770aa66e3cfa7d2f4310a2160a5110159f3
Jiali Yao <Jiali.Yao@emc.com>
2015-05-14 06:19:25 +0000

    Refine performance script
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226735]
    
    
    Former-commit-id: 47e3b741e2d3fa6d145fff11b7d82c4eb4ecd90f

--- performance_full.sh
+++ performance_full.sh
@@ -3,11 +3,18 @@
 source ~/.bashrc
 source ~/qa.sh
 
-hawqconfig -c default_segment_num -v 64
-#hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
-hawqconfig -c hawq_resourceenforcer_cpu_enable -v true
+hawqconfig -c default_segment_num -v 128
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 8
+hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
+
 hawq stop cluster -a
 hawq start cluster -a
+psql -d postgres -c "select * from gp_segment_configuration;" >config
+hawqconfig -s default_segment_num >>config
+hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
+hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
+psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
+
 python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
 sleep 10
 python -u lsp.py -s performance_tpch_200g_full -m 30 -a -c > ./performance_tpch_200g.log 2>&1
@@ -16,5 +23,5 @@ sleep 10
 #sleep 10
 python -u lsp.py -s performance_tpch_stream -m 60 -a -c > ./performance_tpch_stream.log 2>&1
 sleep 10
-python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 5 > ./performance_tpch_concurrent.log 2>&1
+python -u lsp.py -s performance_tpch_concurrent -m 60 -a -c -r 4 > ./performance_tpch_concurrent.log 2>&1
 
--- performance_full_bcn.sh
+++ performance_full_bcn.sh
@@ -3,20 +3,26 @@
 source ~/.bashrc
 source ~/qa.sh
 
-hawqconfig -c default_segment_num -v 64
-hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 4
+hawqconfig -c default_segment_num -v 128
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 8
+hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
+
 hawq stop cluster -a
 hawq start cluster -a
+psql -d postgres -c "select * from gp_segment_configuration;" >config
+hawqconfig -s default_segment_num >>config
+hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
+hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
 psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
-sleep 20
+
 
 python -u lsp.py -s performance_tpch_10g  -c  > ./performance_tpch_10g.log 2>&1
-sleep 10
+sleep 5
 python -u lsp.py -s performance_tpch_200g_full  -c > ./performance_tpch_200g.log 2>&1
-sleep 10
+sleep 5
 #python -u lsp.py -s performance_xmarq_200g  > ./performance_xmarq_200g.log 2>&1
 #sleep 10
 python -u lsp.py -s performance_tpch_stream  -c > ./performance_tpch_stream.log 2>&1
-sleep 10
+sleep 5
 python -u lsp.py -s performance_tpch_concurrent  -c  > ./performance_tpch_concurrent.log 2>&1
 
