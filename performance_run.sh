05987334153c3416d4696eb0bbd916ff2282b607
Jiali Yao <Jiali.Yao@emc.com>
2015-05-20 04:57:23 +0000

    run tpcds
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226766]
    
    
    Former-commit-id: 0688e372beb6661525b3efe4ad2d720ecb72b473

--- performance_run.sh
+++ performance_run.sh
@@ -3,7 +3,19 @@
 source ~/.bashrc
 source ~/qa.sh
 
-hawqconfig -c default_segment_num -v 64
+
+hawqconfig -c default_segment_num -v 128
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 8
+hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
+
 hawq stop cluster -a
 hawq start cluster -a
-python -u lsp.py -s performance_run -a -c  > ./performance_tpch_run.log 2>&1
+psql -d postgres -c "select * from gp_segment_configuration;" >config
+hawqconfig -s default_segment_num >>config
+hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
+hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
+psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
+sleep 10
+
+python -u lsp.py -s tpcds >tpcds 2>&1 
+
