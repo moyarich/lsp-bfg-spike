1af955453713b86d96107e2458c04f28ee439d4e
Jiali Yao <Jiali.Yao@emc.com>
2015-06-01 07:00:18 +0000

    add test for debug
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226819]
    
    
    Former-commit-id: 985efe61ed31765695212956740fc4ce60ca3205

---
+++ performance_test.sh
@@ -0,0 +1,22 @@
+s!/bin/sh
+
+source ~/.bashrc
+source ~/qa.sh
+
+hawqconfig -c default_segment_num -v 128
+hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 8
+hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
+
+hawq stop cluster -a
+hawq start cluster -a
+psql -d postgres -c "select * from gp_segment_configuration;" >config
+hawqconfig -s default_segment_num >>config
+hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
+hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
+psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
+sleep 10
+
+python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
+sleep 10
+
+
