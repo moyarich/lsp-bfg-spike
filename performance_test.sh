5295c61810c8d94d71342109a2fcdefb6ece2660
Jiali Yao <Jiali.Yao@emc.com>
2015-06-01 07:28:18 +0000

    refine performance test
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226821]
    
    
    Former-commit-id: 30b871fd469de280243e2420e17b3f830f84e143

--- performance_test.sh
+++ performance_test.sh
@@ -16,7 +16,7 @@ hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
 psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
 sleep 10
 
-python -u lsp.py -s performance_tpch_10g -m 5 -a -c -r 1  > ./performance_tpch_10g.log 2>&1
+python -u lsp.py -s performance_tpch_10g_sanity -m 5 -a -c -r 1  > ./performance_tpch_10g.log 2>&1
 sleep 10
 
 
