b1a586a312aa44373bc6d91c006e289cce3d3c84
Jiali Yao <Jiali.Yao@emc.com>
2015-06-02 03:01:21 +0000

    refine schedule for resource quene
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226835]
    
    
    Former-commit-id: 31c6d732a594b85ca5303beb39bcc20ea2126fe3

--- performance_resourcequeue.sh
+++ performance_resourcequeue.sh
@@ -1,15 +1,15 @@
-#### only load data using gpadmin
-#python -u lsp.py -s resourcequene_run  > ./resourcequene_run 2>&1
+###Prepare datavvi resourcequene_sri_multiple
+python -u lsp.py -s resourcequene_load >./resourcequene_sri_multiple 2>&1
 
-#load data to run workload
-python -u lsp.py -s resourcequene_tpch_multiplelevel  > ./resourcequene_tpch_multiplelevel 2>&1
+#reuse data and  run workload
+python -u lsp.py -s resourcequene_tpch_multiplelevel -a -c   > ./resourcequene_tpch_multiplelevel 2>&1
 
 #4. 2. 1 ratio concurrent to run tpch 10G 
-python -u lsp.py -s resourcequene_tpch_ratio_10g  > ./resourcequene_tpch_ratio_10g 2>&1
+python -u lsp.py -s resourcequene_tpch_ratio_10g -a -c  > ./resourcequene_tpch_ratio_10g 2>&1
 
 
 #4. 2. 1 ratio concurrent to run tpch 200G 
-python -u lsp.py -s resourcequene_tpch_ratio_200g  > ./resourcequene_tpch_ratio_200g 2>&1
+python -u lsp.py -s resourcequene_tpch_ratio_200g -a -c > ./resourcequene_tpch_ratio_200g 2>&1
 
 #100 stream run single row insert
 python -u lsp.py -s resourcequene_sri_multiple   > ./resourcequene_sri_multiple 2>&1
