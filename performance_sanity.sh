8d414c80c87439bfa12a17db8c109d3b59af34dc
Jiali Yao <Jiali.Yao@emc.com>
2015-06-16 06:33:25 +0000

    refine workload sequence
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 226893]
    
    
    Former-commit-id: d6f0e81b35c55dca0833407685e68e4283c2a613

--- performance_sanity.sh
+++ performance_sanity.sh
@@ -16,8 +16,6 @@ hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
 psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
 sleep 10
 
-python -u lsp.py -s tpcds_sanity -m 10 -a -c  > ./tpcds 2>&1
-sleep 10
 python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
 sleep 10
 python -u lsp.py -s performance_tpch_200g_sanity -m 30 -a -c  > ./performance_tpch_200g.log 2>&1
@@ -25,6 +23,8 @@ sleep 10
 python -u lsp.py -s performance_tpch_concurrent_sanity -m 60 -a -c > ./performance_tpch_concurrent.log 2>&1
 sleep 10
 #4. 2. 1 ratio concurrent to run tpch 10G 
-python -u lsp.py -s resourcequene_tpch_ratio_10g -m 30 -a -c -r 5  > ./resourcequene_tpch_ratio_10g 2>&1
+python -u lsp.py -s resourcequene_tpch_ratio_10g -m 30 -a -c   > ./resourcequene_tpch_ratio_10g 2>&1
 
+python -u lsp.py -s tpcds_sanity -m 10 -a -c -r 5 > ./tpcds 2>&1
+sleep 10
 
