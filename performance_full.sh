76aeba95fdff65c88aa955c496d5863ef8d5d6fd
Jiali Yao <Jiali.Yao@emc.com>
2015-08-01 05:49:01 +0000

    refine performance schedule
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 227339]
    
    
    Former-commit-id: 276ffd5c7d9965e9a8a612922f2e944d09cab586

--- performance_full.sh
+++ performance_full.sh
@@ -3,21 +3,12 @@
 source ~/.bashrc
 source ~/qa.sh
 
-hawqconfig -c default_segment_num -v 128
-hawqconfig -c hawq_resourcemanager_query_vsegment_number_per_segment_limit -v 8
-hawqconfig -c hawq_resourceenforcer_cpu_enable -v false
-
-hawq stop cluster -a
-hawq start cluster -a
 psql -d postgres -c "select * from gp_segment_configuration;" >config
 hawqconfig -s default_segment_num >>config
 hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
 hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
 psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
 
-### TPCDS
-python -u lsp.py -s tpcds -m 10 -a -c  > ./tpcds 2>&1
-sleep 10
 
 ##### TPCH
 python -u lsp.py -s performance_tpch_10g -m 5 -a -c  > ./performance_tpch_10g.log 2>&1
@@ -25,6 +16,9 @@ sleep 10
 python -u lsp.py -s performance_tpch_200g -m 30 -a -c > ./performance_tpch_200g.log 2>&1
 sleep 10
 python -u lsp.py -s performance_xmarq_200g -m 10 -a > ./performance_xmarq_200g.log 2>&1
+sleep 10
+### TPCDS
+python -u lsp.py -s tpcds -m 10 -a -c  > ./tpcds 2>&1
 sleep 10
 python -u lsp.py -s performance_tpch_stream -m 60 -a -c > ./performance_tpch_stream.log 2>&1
 sleep 10
