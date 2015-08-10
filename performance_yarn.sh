a83e90923709585597c3a928c1f303aa3208c9b5
Jiali Yao <Jiali.Yao@emc.com>
2015-08-10 18:19:44 +0000

    add performance yarn
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 227399]
    
    
    Former-commit-id: 8ea1bf8a179b66265ca0043ea628750e3a02834e

---
+++ performance_yarn.sh
@@ -0,0 +1,22 @@
+gva-w7
+#!/bin/sh
+
+source ~/.bashrc
+source ~/qa.sh
+
+psql -d postgres -c "select * from gp_segment_configuration;" >config
+hawqconfig -s default_segment_num >>config
+hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
+hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
+psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
+
+
+python -u lsp.py -s performance_tpch_10g  -a -c  > ./performance_tpch_10g.log 2>&1
+sleep 5
+python -u lsp.py -s performance_tpch_200g  -a -c > ./performance_tpch_200g.log 2>&1
+sleep 5
+
+## Resource quene
+python -u lsp.py -s resourcequene_tpch_ratio_10g -a -c  > ./resourcequene_tpch_ratio_10g 2>&1
+
+
