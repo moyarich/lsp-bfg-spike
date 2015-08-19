ffd2d847b0f9a64131cdf996ae5b4944deb92383
Ruilong Huo <Ruilong.Huo@emc.com>
2015-08-19 10:32:48 +0000

    Add workaround for resource manager timeout too short issue with YARN mode
    [git-p4: depot-paths = "//gpsql/private/liuq8/test/lsp/": change = 227463]
    
    
    Former-commit-id: a41df0b26757cece50ddc5d606cb715f2c45a84c

--- performance_yarn.sh
+++ performance_yarn.sh
@@ -3,10 +3,14 @@
 source ~/.bashrc
 source ~/qa.sh
 
+hawqconfig -c hawq_resourcemanager_resource_timeout -v 3600
+hawq restart cluster -a
+
 psql -d postgres -c "select * from gp_segment_configuration;" >config
 hawqconfig -s default_segment_num >>config
 hawqconfig -s hawq_resourcemanager_query_vsegment_number_per_segment_limit >>config
 hawqconfig -s hawq_resourceenforcer_cpu_enable >>config
+hawqconfig -s hawq_resourcemanager_resource_timeout >>config
 psql -d postgres -c "drop table if exists test; create table test(a int); insert into test values (1);"
 
 
